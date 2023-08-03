ae977b7edda8c4a6abe5a073fef86be3bdc2a668
Agend047 <137612856+Agend047@users.noreply.github.com>
2023-08-03 09:13:03 +0200

    Add files via upload
    Finished Quiz after a Week

---
+++ questionData.js
@@ -0,0 +1,100 @@
+let dataHTML = [
+    {
+        'question': "Wer hat HTML erfunden?",
+        'answer_1': 'Robbie Williams',
+        'answer_2': 'Lady Gaga',
+        'answer_3': 'Tim Berners-Lee',
+        'answer_4': 'Justin Biber',
+        'right_answer': 3,
+    },
+    {
+        'question': "Welches Attribut kann man NICHT für Textarea verwenden?",
+        'answer_1': 'readonly',
+        'answer_2': 'max',
+        'answer_3': 'from',
+        'answer_4': 'spellcheck',
+        'right_answer': 1,
+    },
+    {
+        'question': "Was bedeutet das HTML Tag 'a'?",
+        'answer_1': 'Text Fett',
+        'answer_2': 'Ein Link',
+        'answer_3': 'Container',
+        'answer_4': 'Text Kursiv',
+        'right_answer': 2,
+    },
+]
+
+let dataCSS = [
+    {
+        'question': "Wer hat CSS erfunden?",
+        'answer_1': 'Hakon Wium Lie',
+        'answer_2': 'Lady Gaga',
+        'answer_3': 'Tim Berners-Lee',
+        'answer_4': 'Justin Biber',
+        'right_answer': 1,
+    },
+    {
+        'question': "Wie bearbeitet man in CSS eine Klasse",
+        'answer_1': '"name{}";',
+        'answer_2': 'name{};',
+        'answer_3': '.name{}',
+        'answer_4': '#name{}',
+        'right_answer': 3,
+    },
+    {
+        'question': "Wie definiert man in CSS eine Hintergrundfarbe?",
+        'answer_1': 'color: white;',
+        'answer_2': 'background-color: red;',
+        'answer_3': 'bg-color green;',
+        'answer_4': 'bg_color: black;',
+        'right_answer': 2,
+    },
+    {
+        'question': "Was bedeutet das CSS Tag 'margin'?",
+        'answer_1': 'Text Fett',
+        'answer_2': 'Ein Link',
+        'answer_3': 'Container',
+        'answer_4': 'unsichtbarer Platz um ein HTML Element',
+        'right_answer': 4,
+    },
+];
+
+let dataJS = [
+    {
+        'question': "Wie definiert man in JavaScript eine Variable",
+        'answer_1': 'let 100 = rate;',
+        'answer_2': 'let rate = 100;',
+        'answer_3': 'rate = 100;',
+        'answer_4': '100 = let rate;',
+        'right_answer': 2,
+    },
+    {
+        'question': "Was bedeutet das JS Tag 'function'?",
+        'answer_1': 'Nix',
+        'answer_2': 'Was?',
+        'answer_3': 'Bier leer!',
+        'answer_4': 'Eine Funktion wird angelegt',
+        'right_answer': 4,
+    },
+];
+
+
+let dataJava = [
+    {
+        'question': "Wer braucht Java?",
+        'answer_1': 'Ich nicht!',
+        'answer_2': 'z.B. Müller',
+        'answer_3': 'Niemand!',
+        'answer_4': 'Manche',
+        'right_answer': 4,
+    },
+    {
+        'question': "Wann wurde Java eingeführt?",
+        'answer_1': '1995',
+        'answer_2': 'Im Mittelalter',
+        'answer_3': '2005',
+        'answer_4': '1884',
+        'right_answer': 1,
+    }
+];
